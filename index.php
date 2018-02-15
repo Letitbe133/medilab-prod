7fce76574fa64e0721b9eabbede3575fae15e4ab
miplayer1 <mhiezely@hotmail.fr>
2018-02-15 14:41:43 +0100

    piwik

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
 	<meta property="og:description" content="We bring populations closer to quality products. We continuously strive to offer the best deal, in compliance with existing standards, by offering guarantees on the origin and quality of medicinal compounds." />
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="http://www.medilab.one/assets/og-img.png" />
+	<!--Matomo-->
+	<script type="text/javascript">var _paq=_paq||[];_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function(){var u="//analytics.mpaw.xyz/piwik/";_paq.push(['setTrackerUrl',u+'piwik.php']);_paq.push(['setSiteId','8']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.type='text/javascript';g.async=true;g.defer=true;g.src=u+'piwik.js';s.parentNode.insertBefore(g,s);})();</script>
+	<!--End Matomo Code-->
   </head>
   <body>
 <!-- Header start -->
