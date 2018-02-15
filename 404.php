55b240ba6fbd1bec0c3ac2506779d77b4446fad5
miplayer1 <mhiezely@hotmail.fr>
2018-02-15 14:15:19 +0100

    pages 404/403

---
+++ 404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<!--[if lt IE 9]>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script> 
+	<![endif]-->
+    <title>Page not found - Medilab</title>
+    <meta name="viewport" content="width=device-width initial-scale=1 user-scalable=no">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Poppins:300,400,400italic,600,700|Open+Sans:300,400,600,700,800,900" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+    <link rel="Shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <style>
+    .padding-top {padding-top: 50px;}
+	 </style>
+</head>    
+<body>
+<div class="row padding-top">
+<div class="col s12 m6 offset-m3  ">
+<a href="index.php" ><img src="assets/img/logos/logo-medilab.svg" alt="Retour à l'accueil"></a>
+</div>
+<div class="col s12 m6 offset-m3  ">
+<h1 class="grey-text">PAGE NOT FOUND</h1>
+</div>
+<div class="col s12 m6 offset-m3 ">
+<p class="grey-text">Seems like the page you requested is no longer here.</p>
+</div>
+</div>
+</body>
+</html>
